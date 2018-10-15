985f0bd7cb5c68076914ab15d39392210bc1f94e
Aidan Lowson <aidanlowson@hotmail.co.uk>
2018-10-15 19:42:15 +0100

    Webpack tidy up and adding sass to prod build

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -192,24 +192,6 @@ module.exports = {
               },
             ],
           },
-        //   {
-        //     test: /\.scss$/,
-        //     use: [
-        //       {
-        //         loader: "style-loader"
-        //       },
-        //       {
-        //         loader: "css-loader"
-        //       },
-        //       {
-        //         loader: "sass-loader",
-        //           options: {
-        //             modules: true,
-        //             localIdentName: '[name]__[local]___[hash:base64:5]',
-        //           },
-        //       },
-        //   ],
-        // },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
